0969d23643984f40887766842848e0e992efc03e
Jake Armour <me@jakearmour.co.uk>
2019-07-27 00:35:01 +0100

    Final changes for tonight, useless commit messages

--- src/components/spotify/InfoPanel.vue
+++ src/components/spotify/InfoPanel.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
   .info-panel(v-if="track")
-    img(v-if="albumCover" :src="albumCover.url")
+    br
     .columns
-      .column
-        p.track {{ track.track.name }}
-        p.artist {{ track.track.artists.map(a => a.name).join(', ') }}
+      .column.is-1
+        br
+      .column.is-9
+        img(v-if="albumCover" :src="albumCover.url")
+    .columns
+      .column.is-10
+        p.track {{ track.track.name }} - {{ track.track.artists.map(a => a.name).join(', ') }}
         p.album {{ track.track.album.name }}
-      .column
-        b-icon(pack="far" icon="clock")
+      .column.is-2
+        b-icon(icon="clock")
         p {{ millis }}
     a(:href="track.track.uri") {{ "Open in Spotify" }}
     b-taglist(attached=true)
